f8c622c2f84ec72f8df32e1bba36aad3f608d669
Laurah-n <laura.hn317@gmail.com>
2024-09-16 15:19:37 +0000

    studentmachine send

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
+	fstream plik;
+	plik.open("dane.txt", ios::out);
 	double a;
 	cout << "podaj liczbe:" << endl;
 	cin >> a;
 	while(a>0.49){
 		a = a - 0.5;
 	cout << a << endl;
+	plik << a << endl;
 	}
+	plik.close();
 	return 0;
 }
