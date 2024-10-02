8c1dea7e6e1d9dd4fa072fe5e136a002b451b33b
Laurah-n <student@student.machine>
2024-10-02 08:15:01 +0000

    studentmachine send

--- cpp/babelkowe/main.cpp
+++ cpp/babelkowe/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-	cout<<"witaj swiecie"<<endl;
+void sortowanie_babelkowe(int tab[], int n)
+{
+	for(int i=0; i<n; i++)
+		for(int j=1; j<n-i; j++)
+		if(tab[j-1]>tab[j])
+			swap(tab[j-1], tab[j]);
+}
+
+int main()
+{
+	int *tab, n;
+
+	cout<<"ile liczb?";
+	cin>>n;
+
+	tab = new int[n];
+
+	for(int i=0;i<n;i++)
+		cin>>tab[i];
+
+	sortowanie_babelkowe(tab,n);
+
+	for(int i=0; i<n; i++)
+		cout<<tab[i]<<" ";
+
 	return 0;
 }
