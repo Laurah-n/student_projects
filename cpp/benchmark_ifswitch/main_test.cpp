dff325da1a8881f8220aacf048ce079f793e26ef
Laurah-n <laura.hn317@gmail.com>
2024-11-19 23:04:04 +0100

    Update main_test.cpp

--- cpp/benchmark_ifswitch/main_test.cpp
+++ cpp/benchmark_ifswitch/main_test.cpp
@@ -1,93 +1,89 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <ctime>
 #include "gtest/gtest.h"
 #include <chrono>
 
-// Function to simulate if statements with a single user-provided value.
-void testIf(int value, int userValue) {
-    if (value == userValue) {}
-    else if (value == userValue + 1) {}
-    else if (value == userValue + 2) {}
-    else if (value == userValue + 3) {}
-    else if (value == userValue + 4) {}
-    else if (value == userValue + 5) {}
-    else if (value == userValue + 6) {}
-    else if (value == userValue + 7) {}
-    else if (value == userValue + 8) {}
-    else if (value == userValue + 9) {}
-}
+using namespace std;
 
-// Function to simulate switch/case statements with a single user-provided value.
-void testSwitch(int value, int userValue) {
-    switch (value) {
-        case 0: break;
-        case 1: break;
-        case 2: break;
-        case 3: break;
-        case 4: break;
-        case 5: break;
-        case 6: break;
-        case 7: break;
-        case 8: break;
-        case 9: break;
-        default: break;
+int day = 1;
+
+void test_if(int day) {
+    if (day == 1) {
+        cout << "Monday";
+    } else if (day == 2) {
+        cout << "Tuesday";
+    } else if (day == 3) {
+        cout << "Wednesday";
+    } else if (day == 4) {
+        cout << "Thursday";
+    } else if (day == 5) {
+        cout << "Friday";
+    } else if (day == 6) {
+        cout << "Saturday";
+    } else if (day == 7) {
+        cout << "Sunday";
     }
 }
 
-TEST(BenchmarkTest, IfStatementBenchmark) {
-    std::srand(std::time(0));
-
-    // Pobranie jednej wartości od użytkownika
-    int userValue;
-    std::cout << "Podaj jedną wartość do testu if: ";
-    std::cin >> userValue;
-
-    // Zmienna do sumowania czasu
-    std::chrono::duration<double> total_time_if(0);
-
-    for (int i = 0; i < 10000; i++) {
-        int randomValue = std::rand() % 10;
-
-        auto start = std::chrono::high_resolution_clock::now();
-        testIf(randomValue, userValue);
-        auto end = std::chrono::high_resolution_clock::now();
-
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        total_time_if += elapsed_seconds;
+void test_case(int day){
+    switch (day) {
+      case 1:
+        cout << "Monday";
+        break;
+      case 2:
+        cout << "Tuesday";
+        break;
+      case 3:
+        cout << "Wednesday";
+        break;
+      case 4:
+        cout << "Thursday";
+        break;
+      case 5:
+        cout << "Friday";
+        break;
+      case 6:
+        cout << "Saturday";
+        break;
+      case 7:
+        cout << "Sunday";
+        break;
     }
-
-    // Wyświetlenie sumarycznego czasu dla wszystkich iteracji
-    std::cout << "Suma czasu wykonania testIf dla 10000 iteracji: " << total_time_if.count() << "s\n";
 }
 
-TEST(BenchmarkTest, SwitchCaseBenchmark) {
-    std::srand(std::time(0));
+TEST(BenchmarkTest, IfBenchmark)
+{
+	auto start = chrono::high_resolution_clock::now();
 
-    // Pobranie jednej wartości od użytkownika
-    int userValue;
-    std::cout << "Podaj jedną wartość do testu switch/case: ";
-    std::cin >> userValue;
+    test_if(day);
 
-    // Zmienna do sumowania czasu
-    std::chrono::duration<double> total_time_switch(0);
+	auto end = chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < 10000; i++) {
-        int randomValue = std::rand() % 10;
+	chrono::duration<double> elapsed_seconds = end - start;
 
-        auto start = std::chrono::high_resolution_clock::now();
-        testSwitch(randomValue, userValue);
-        auto end = std::chrono::high_resolution_clock::now();
-
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        total_time_switch += elapsed_seconds;
-    }
-
-    // Wyświetlenie sumarycznego czasu dla wszystkich iteracji
-    std::cout << "Suma czasu wykonania testSwitch dla 10000 iteracji: " << total_time_switch.count() << "s\n";
+	cout << "Czas dla instrukcji if: " << elapsed_seconds.count() << "s" << endl;
 }
 
-int main(int argc, char **argv) {
+TEST(BenchmarkTest, CaseBenchmark)
+{
+	auto start = chrono::high_resolution_clock::now();
+
+    test_case(day);
+
+	auto end = chrono::high_resolution_clock::now();
+
+	chrono::duration<double> elapsed_seconds = end - start;
+
+    cout << "Czas dla instrukcji case: " << elapsed_seconds.count() << "s" << endl;
+}
+
+int main(int argc, char **argv)
+{
+	cout << "Podaj liczbe (1-7): ";
+	cin >> day;
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
