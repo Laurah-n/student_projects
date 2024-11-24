10ecad2473e0955f0120253d444bd7d5d4863dec
Laurah-n <laura.hn317@gmail.com>
2024-11-24 22:28:19 +0100

    Create functions.h

---
+++ cpp/schody/functions.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void wprowadzenieWymiarow(double& wysokosc, double& dlugosc);
+int obliczIloscStopni(double wysokosc, double dlugosc);
+void obliczWymiaryStopnia(double dlugosc, double wysokosc, int iloscStopni, double& dlugoscStopnia, double& wysokoscStopnia);
+
+#endif
