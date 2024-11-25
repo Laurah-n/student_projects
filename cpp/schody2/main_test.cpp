e9ead23ae5d6b517bf9e8c714a2760246059d160
Laurah-n <laura.hn317@gmail.com>
2024-11-25 16:51:07 +0100

    Update main_test.cpp

--- cpp/schody2/main_test.cpp
+++ cpp/schody2/main_test.cpp
@@ -1,36 +1,28 @@
-// main_test.cpp
 #include <gtest/gtest.h>
+#include "functions.h"
 #include "functions.cpp"
-#include <string>
 
-TEST(FizzBuzzTests, ReturnsFizzForMultipleOf3) {
- //#TODO: tu napisz test
-	string result = FizzBuzz(3);
-	EXPECT_EQ(result, "Fizz");
+TEST(SchodyTests, TestIloscStopni){
+	EXPECT_EQ(obliczIloscStopni(150, 200), 7);
+	EXPECT_EQ(obliczIloscStopni(180, 280), 10);
+	EXPECT_EQ(obliczIloscStopni(50, 70), -1);
 }
 
-TEST(FizzBuzzTests, ReturnsBuzzForMultipleOf5) {
-  //#TODO: tu napisz test
-	string result = FizzBuzz(5);
-	EXPECT_EQ(result, "Buzz");
+TEST(SchodyTests, TestWymiarySchodka){
+	double dlugoscStopnia, wysokoscStopnia;
+
+	obliczWymiaryStopnia(300.0, 150.0, 10, dlugoscStopnia, wysokoscStopnia);
+	EXPECT_DOUBLE_EQ(dlugoscStopnia, 30.0);
+	EXPECT_DOUBLE_EQ(wysokoscStopnia, 15.0);
+
+	obliczWymiaryStopnia(200.0, 150.0, 7, dlugoscStopnia, wysokoscStopnia);
+	EXPECT_DOUBLE_EQ(dlugoscStopnia, 28.57);
+	EXPECT_DOUBLE_EQ(wysokoscStopnia, 21.43);
+
+	EXPECT_THROW(obliczWyiaryStopnia(300.0, 150.0, -1, dlugoscStopnia, wysokoscStopnia), std::invalid_argument;
 }
 
-TEST(FizzBuzzTests, ReturnsFizzBuzzForMultipleOf15) {
-  //#TODO: tu napisz test
-	string result = FizzBuzz(15);
-	EXPECT_EQ(result, "FizzBuzz");
+int main(int argc, char **argv){
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
-
-TEST(FizzBuzzTests, ReturnsEmptyStringForNonMultipleOf3Or5) {
-    string result = FizzBuzz(7);
-    EXPECT_EQ(result, "");
-}
-
-
-// ... inne testy ...
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
