c4872f1563fd80ffdf0ad899c2ee04e3a186957d
Laurah-n <laura.hn317@gmail.com>
2024-11-25 16:51:24 +0100

    Update main.cpp

--- cpp/schody2/main.cpp
+++ cpp/schody2/main.cpp
@@ -1,10 +1,23 @@
-+#include <iostream>
-#include "functions.cpp"
+#include <iostream>
+#include "functions.h"
 
-using namespace std;
+int main() {
+    double wysokosc, dlugosc;
+    wprowadzenieWymiarow(wysokosc, dlugosc);
 
-int main(){
+    int iloscStopni = obliczIloscStopni(wysokosc, dlugosc);
+
+    if (iloscStopni == -1) {
+        std::cerr << "Nie można wygenerować schodów o podanych wymiarach." << std::endl;
+        return 1;
+    }
+
+    double dlugoscStopnia, wysokoscStopnia;
+    obliczWymiaryStopnia(dlugosc, wysokosc, iloscStopni, dlugoscStopnia, wysokoscStopnia);
+
+    std::cout << "Ilość stopni schodów: " << iloscStopni << std::endl;
+    std::cout << "Długość pojedynczego stopnia: " << dlugoscStopnia << " cm" << std::endl;
+    std::cout << "Wysokość pojedynczego stopnia: " << wysokoscStopnia << " cm" << std::endl;
 
     return 0;
 }
-
