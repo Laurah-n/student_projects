3d79fa818a7d4f4f200396785970a79c3bf6898b
Laurah-n <laura.hn317@gmail.com>
2024-12-02 15:49:54 +0100

    Delete cpp/schody directory

--- cpp/schody/functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-void wprowadzenieWymiarow(double& wysokosc, double& dlugosc);
-int obliczIloscStopni(double wysokosc, double dlugosc);
-void obliczWymiaryStopnia(double dlugosc, double wysokosc, int iloscStopni, double& dlugoscStopnia, double& wysokoscStopnia);
-
-#endif
