578408d1788b32b4489fa2493dbdcdf0da1e7998
Laurah-n <laura.hn317@gmail.com>
2024-12-02 16:43:16 +0100

    Update Kalkulator.cs

--- cpp/SchodyApp/Kalkulator.cs
+++ cpp/SchodyApp/Kalkulator.cs
@@ -1,1 +1,6 @@
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtHeight.Text, out double wysokosc) || !double.TryParse(txtLength.Text, out double dlugosc))
+            {
 
+            }
