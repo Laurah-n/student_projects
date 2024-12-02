75b098c0beae0a04bdf219e27c9a3f4e6fc46498
Laurah-n <laura.hn317@gmail.com>
2024-12-02 17:05:00 +0100

    Update Kalkulator.cs

--- cpp/SchodyApp/Kalkulator.cs
+++ cpp/SchodyApp/Kalkulator.cs
@@ -4,3 +4,55 @@
             {
 
             }
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchodyApp
+{
+    public partial class Kalkulator : Program
+    {
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+
+        private int ObliczIloscStopni(double wysokosc, double dlugosc)
+        {
+            const double minWysokosc = 15.0;
+            const double maxWysokosc = 17.0;
+            const double minDlugosc = 28.0;
+            const double maxDlugosc = 30.0;
+
+            int iloscStopniWysokosc = (int)Math.Floor(wysokosc / minWysokosc);
+            int iloscStopniDlugosc = (int)Math.Floor(dlugosc / minDlugosc);
+
+            if (iloscStopniWysokosc <= 0 || iloscStopniDlugosc <= 0)
+            {
+                return -1;
+            }
+
+            int iloscStopni = Math.Min(iloscStopniWysokosc, iloscStopniDlugosc);
+
+            double dlugoscStopnia = dlugosc / iloscStopni;
+            if (dlugoscStopnia > maxDlugosc)
+            {
+                return -1;
+            }
+
+            return iloscStopni;
+        }
+
+        private void ObliczWymiaryStopnia(double dlugosc, double wysokosc, int iloscStopni, out double dlugoscStopnia, out double wysokoscStopnia)
+        {
+            dlugoscStopnia = dlugosc / iloscStopni;
+            wysokoscStopnia = wysokosc / iloscStopni;
+        }
+    }
+}
